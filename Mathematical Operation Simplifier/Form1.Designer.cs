b0932d50c47bed2139eaf02ad70b99e56d4d94a4
Phemis <phemisz@gmail.com>
2019-11-20 19:21:17 +0300

    Mathematical Operation Simplifier

---
+++ Mathematical Operation Simplifier/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Mathematical_Operation_Simplifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Drag Here";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(6, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(459, 216);
+            this.listBox1.TabIndex = 23;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragEnter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(3, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Not Loaded !";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(310, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 37);
+            this.button2.TabIndex = 21;
+            this.button2.TabStop = false;
+            this.button2.Text = "Simplify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(12, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 37);
+            this.button1.TabIndex = 20;
+            this.button1.TabStop = false;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(201, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "RհօԵɑѵ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(37, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 42);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Mathematical Operations Simplifier\r\n                 by";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(477, 364);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mathematical Operations Simplifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Mathematical Operation Simplifier/Form1.cs
@@ -0,0 +1,217 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Mathematical_Operation_Simplifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        #region Variables
+        string directoryName = "";
+        string filePath = "";
+        static ModuleDefMD module = null;
+        public Thread thr;
+        #endregion
+
+        #region Github
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Rhotav");
+        }
+        #endregion
+
+        #region Save
+        static void SaveAssembly()
+        {
+            var writerOptions = new NativeModuleWriterOptions(module, true);
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+            writerOptions.MetadataOptions.Flags = (MetadataFlags.PreserveTypeRefRids | MetadataFlags.PreserveTypeDefRids | MetadataFlags.PreserveFieldRids | MetadataFlags.PreserveMethodRids | MetadataFlags.PreserveParamRids | MetadataFlags.PreserveMemberRefRids | MetadataFlags.PreserveStandAloneSigRids | MetadataFlags.PreserveEventRids | MetadataFlags.PreservePropertyRids | MetadataFlags.PreserveTypeSpecRids | MetadataFlags.PreserveMethodSpecRids | MetadataFlags.PreserveStringsOffsets | MetadataFlags.PreserveUSOffsets | MetadataFlags.PreserveBlobOffsets | MetadataFlags.PreserveAll | MetadataFlags.AlwaysCreateGuidHeap | MetadataFlags.PreserveExtraSignatureData | MetadataFlags.KeepOldMaxStack);
+            module.NativeWrite(Path.GetDirectoryName(module.Location) + @"\" + Path.GetFileNameWithoutExtension(module.Location) + "_simplfied.exe", writerOptions);
+        }
+
+        #endregion
+
+        #region Select
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Executable Files|*.exe|DLL Files |*.dll";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    module = ModuleDefMD.Load(open.FileName);
+                    filePath = open.FileName;
+                    label4.Text = "Loaded !";
+                    label4.ForeColor = Color.Lime;
+                    listBox1.Items.Clear();
+                    label3.Visible = false;
+                    listBox1.Items.Add("File is loaded !");
+                    listBox1.Items.Add("EntryPoint MDToken : 0x" + module.EntryPoint.MDToken.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                filePath = "";
+                module = null;
+                MessageBox.Show(ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label4.Text = "Not Loaded !";
+                label4.ForeColor = Color.Lime;
+                label3.Visible = true;
+
+            }
+        }
+        #endregion
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (filePath != string.Empty && module != null)
+            {
+                thr = new Thread(new ThreadStart(CodeBlock));
+                thr.Start();
+            }
+        }
+        public void CodeBlock()
+        {
+            try
+            {
+                listBox1.Items.Add("Mathematical operations simplifying..");
+                foreach (TypeDef type in module.Types)
+                {
+                    foreach (MethodDef method in type.Methods)
+                    {
+                        if (!method.HasBody) continue;
+
+                        for (int i = 0; i < method.Body.Instructions.Count; i++)
+                        {
+                            if (method.Body.Instructions[i].OpCode == OpCodes.Ldc_I4 &&
+                                method.Body.Instructions[i + 1].OpCode == OpCodes.Ldc_I4 &&
+                                method.Body.Instructions[i + 3].OpCode == OpCodes.Ldc_I4)
+                            {
+                                int one = method.Body.Instructions[i].GetLdcI4Value();
+                                int two = method.Body.Instructions[i + 1].GetLdcI4Value();
+                                int three = method.Body.Instructions[i + 3].GetLdcI4Value();
+                                string opOne = islemDondur(method.Body.Instructions[i + 2].OpCode.ToString());
+                                string opTwo = islemDondur(method.Body.Instructions[i + 4].OpCode.ToString());
+
+                                int value = (int)(Eval(one, opOne, two, opTwo, three));
+
+                                method.Body.Instructions[i].OpCode = OpCodes.Ldc_I4;
+                                method.Body.Instructions[i].Operand = value;
+                                method.Body.Instructions.RemoveAt(i + 1);
+                                method.Body.Instructions.RemoveAt(i + 1);
+                                method.Body.Instructions.RemoveAt(i + 1);
+                                method.Body.Instructions.RemoveAt(i + 1);
+                            }
+                        }
+                    }
+                }
+                SaveAssembly();
+                listBox1.Items.Add("Compeleted and Saved !");
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.Message);
+            }
+        }
+
+
+        #region Math
+        public static string islemDondur(string deger)
+        {
+            switch (deger)
+            {
+                case "div":
+                    return "/";
+                case "sub":
+                    return "-";
+                case "add":
+                    return "+";
+            }
+            return "";
+        }
+        public static double Eval(int bir, string islemBir, int iki, string islemİki, int uc)
+        {
+            string islem = bir.ToString() + islemBir + iki.ToString() + islemİki + uc.ToString();
+
+            double sonuc = Convert.ToDouble(new DataTable().Compute(islem, null));
+
+            return sonuc;
+        }
+        #endregion
+
+        #region DragDrop
+        private void ListBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".");
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            int num2 = text.LastIndexOf("\\");
+                            if (num2 != -1)
+                            {
+                                directoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (directoryName.Length == 2)
+                            {
+                                directoryName += "\\";
+                            }
+                            module = ModuleDefMD.Load(text);
+                            filePath = text;
+                            label4.Text = "Loaded !";
+                            label4.ForeColor = Color.Lime;
+                            label3.Visible = false;
+                            listBox1.Items.Clear();
+                            listBox1.Items.Add("File is loaded !");
+                            listBox1.Items.Add("EntryPoint MDToken : 0x" + module.EntryPoint.MDToken.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                filePath = "";
+                module = null;
+                MessageBox.Show(ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label4.Text = "Not Loaded !";
+                label4.ForeColor = Color.Red;
+
+            }
+        }
+
+        private void ListBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        #endregion
+    }
+}
